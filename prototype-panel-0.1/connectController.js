d73c07180ac9be099f61b8531c6bac09b349c136
Maayanp <61969711+Maayanp@users.noreply.github.com>
2020-12-16 14:36:33 +0200

    update

--- prototype-panel-0.1/connectController.js
+++ prototype-panel-0.1/connectController.js
@@ -8,19 +8,25 @@ jqueryUI.src = 'https://code.jquery.com/ui/1.12.1/jquery-ui.js';
 jqueryUI.type = 'text/javascript';
 document.getElementsByTagName("head")[0].appendChild(jqueryUI);
 
-// var jqueryUiCss = document.createElement("link");
-// jqueryUiCss.src = 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css';
-// jqueryUiCss.rel = 'stylesheet';
-// document.getElementsByTagName("head")[0].appendChild(jqueryUiCss);
-
 var scrollToView = document.createElement("script");
 scrollToView.src = './scrollToView.js';
 document.getElementsByTagName("head")[0].appendChild(scrollToView);
 
 var controllerStructureFile = document.createElement("script");
+<<<<<<< Updated upstream
 controllerStructureFile.src = './controllerStructure.js';
 document.getElementsByTagName("head")[0].appendChild(controllerStructureFile);
 
+=======
+controllerStructureFile.src = 'https://wix-prototypers.github.io/prototype-panel/prototype-panel-0.1/prototypePanelStructure.js';
+document.getElementsByTagName("head")[0].appendChild(controllerStructureFile);
+
+var prototypePanelCss = document.createElement("link");
+prototypePanelCss.href = 'https://wix-prototypers.github.io/prototype-panel/prototype-panel-0.1/prototypePanel.css';
+prototypePanelCss.rel = 'stylesheet';
+document.getElementsByTagName("head")[0].appendChild(prototypePanelCss);
+
+>>>>>>> Stashed changes
 // --- ??? --- //
 var customFile = document.createElement("script");
 customFile.src = './customController.js';
